02608abb82fe74ad27b5b67c1a529fbe4794f4e7
Crevista <billybickell@hotmail.co.uk>
2025-04-26 21:43:58 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to Clip Search</h1>
+      <p>Search YouTube transcripts and find the perfect clips!</p>
+    </div>
+  );
+}
